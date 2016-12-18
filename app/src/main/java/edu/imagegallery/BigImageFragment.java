faded3bb2e6a84e1e795726130eb0bd2d242886b
Alexandr Meleshko <meleshko_alexandr@mail.ru>
2016-12-18 23:56:44 +0200

    Second Commit

--- app/src/main/java/edu/imagegallery/BigImageFragment.java
+++ app/src/main/java/edu/imagegallery/BigImageFragment.java
@@ -3,13 +3,25 @@ package edu.imagegallery;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class BigImageFragment extends Fragment{
-    private static String url;
+    private static File file;
     static final String ARGUMENT_IMAGE_NUMBER = "arg_page_number";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -17,7 +29,10 @@ public class BigImageFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_big_image, container, false);
         ImageView image = (ImageView)v.findViewById(R.id.iv_big_image);
 
-        url = "file://" + getArguments().getString(ARGUMENT_IMAGE_NUMBER);
+        String filePath = getArguments().getString(ARGUMENT_IMAGE_NUMBER);
+        file = new File(filePath);
+
+        String url = "file://" + filePath;
         SingleFragmentActivity.imageLoader.displayImage(url, image);
         return v;
     }
@@ -28,5 +43,22 @@ public class BigImageFragment extends Fragment{
         arguments.putString(ARGUMENT_IMAGE_NUMBER, index);
         pageFragment.setArguments(arguments);
         return pageFragment;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_delete, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item_delete:
+                if (file.delete()) getActivity().finish();
+                Toast.makeText(getActivity(), "deleted" + file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                return  true;
+            default: return super.onOptionsItemSelected(item);
+        }
     }
 }
